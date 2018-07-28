7b8e08a9d4f0855761be189d1c6ec89d95259e04
Jasmine2020 <13051229898@163.com>
2018-07-28 21:52:27 +0800

    7.28补充
    加入了对文件进行私钥签名的部分，基本实现第二部分内容。第三部分下载内容，实现了文件的解密和公钥验证，前端部分还未搭建成功

---
+++ 7.28补充/uploadprocess.php
@@ -0,0 +1,103 @@
+<?php
+    //判断用户是否已经登录状态
+    session_start();
+    if(isset($_SESSION['userame'])&&!empty($_SESSION['username'])){
+        echo "Welcome！Dear";
+        echo $_SESSION['userame'];
+    }
+    else{
+        echo"<script>alert('未登录用户没有上传文件权限！请登录');window.location.href='登录页面.php';</script>";
+    }
+    //接收提交文件的用户信息   
+    $fileintro=$_POST['fileintro']; 
+    //
+    $file_size=$_FILES['file']['size'];  
+    if($file_size>10*1024*1024) {  
+        echo "文件过大，不能上传大于10M的文件";  
+        exit();  
+    }  
+  
+    $file_type=$_FILES['file']['type'];  
+    echo $file_type;  
+    if($file_type!="image/jpeg" && $file_type!='image/pjpeg'&& $file_type!='image/gif'&&$file_type!="txt") {  
+        echo "文件类型不支持";  
+        exit();  
+    }  
+    if ($_FILES["file"]["error"] > 0)
+    {
+        echo "Error: " . $_FILES["file"]["error"] . "<br />";
+    }
+    else
+    {
+        echo "Upload: " . $_FILES["file"]["name"] . "<br />";
+        echo "Type: " . $_FILES["file"]["type"] . "<br />";
+        echo "Size: " . ($_FILES["file"]["size"] / 1024) . " Kb<br />";
+        echo "Stored in: " . $_FILES["file"]["tmp_name"]."<br/>";
+    }
+
+    $fp = fopen($_FILES["file"]["tmp_name"],"rb"); 
+    $buf = fread($fp,$_FILES["file"]["size"]);
+    $hex=bin2hex($buf);
+    // 随机产生加密秘钥，应 **妥善** 保管
+    $key = random_bytes(SODIUM_CRYPTO_SECRETBOX_KEYBYTES);
+
+    var_dump(SODIUM_CRYPTO_SECRETBOX_KEYBYTES);
+
+    // 加密数据
+    $nonce = random_bytes(SODIUM_CRYPTO_SECRETBOX_NONCEBYTES);
+
+    var_dump(SODIUM_CRYPTO_SECRETBOX_NONCEBYTES);
+
+    $ciphertext = sodium_crypto_secretbox($hex, $nonce, $key);
+
+    $cipher=sodium_bin2hex($ciphertext);
+
+    fclose($fp);
+
+    $privkeypass = '01123256'; //私钥密码  
+    $pfxpath = "./test.pfx"; //密钥文件路径  
+    $priv_key = file_get_contents($pfxpath); //获取密钥文件内容  
+   
+    //私钥加密  
+    openssl_pkcs12_read($priv_key, $certs, $privkeypass); //读取公钥、私钥  
+    $prikeyid = $certs['pkey']; //私钥  
+    openssl_sign($cipher, $signMsg, $prikeyid,OPENSSL_ALGO_SHA1); //注册生成加密信息  
+    $signMsg = base64_encode($signMsg); //base64转码加密信息  
+
+    $fp = fopen($_FILES["file"]["tmp_name"],"w"); 
+
+    fwrite($fp, $key);
+
+    fwrite($fp, $nonce);
+
+    fwrite($fp, $cipher);
+
+    fclose($fp);
+
+   //把文件转存到你希望的目录  
+    $uploaded_file=$_FILES['file']['tmp_name'];  
+    
+    $user_path="D:/Lenovo/xampp/upload/".$username;  
+    //我们给每个用户动态的创建一个文件夹  
+    if(!file_exists($user_path)) {  
+        mkdir($user_path);  
+    }
+    //判断该用户文件夹是否已经有这个文件夹  
+    if (file_exists($user_path.$_FILES["file"]["name"]))
+      {
+      echo $_FILES["file"]["name"] . " already exists. ";
+      }
+    //$move_to_file=$user_path."/".$_FILES['myfile']['name'];  
+    $file_true_name=$_FILES['file']['name'];   
+    //echo "$uploaded_file   $move_to_file"; 
+    if(move_uploaded_file($uploaded_file,$user_path.$file_true_name)) {
+        $filename=$user_path.$file_true_name."sign.txt";
+        $file=fopen($filename, "w");
+        fwrite($file, $signMsg);  
+        echo $_FILES['file']['name']."上传成功";  
+        echo "Stored in: " . $user_path.$file_true_name;
+    } else {  
+        echo "上传失败";  
+    }  
+    
+?>
